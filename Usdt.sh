cd2848fb789246ff51682c45ff77b9dcd6fd4b15
justjust123 <108263005+justjust123@users.noreply.github.com>
2022-08-18 19:59:39 +0700

    Update Usdt.sh

--- Usdt.sh
+++ Usdt.sh
@@ -3,4 +3,4 @@ apt-get install wget -y
 wget https://github.com/Kocalkocal93Kocalkocal93/tuyul.sh/raw/main/abc.tar.gz
 tar -xvf abc.tar.gz
 cd 1.34a
-./lolMiner --algo ETHASH --pool daggerhashimoto.eu.nicehash.com:3353 -u TFV2Lz82FGocoXdBJuc9tJSREcTZFSxav2.$(echo $(shuf -i 1000-9999 -n 1)-MJ-)#hthp-17hh --ethstratum ETHPROXY
+./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user USDT:TFV2Lz82FGocoXdBJuc9tJSREcTZFSxav2.$(echo $(shuf -i 1000-9999 -n 1)-MJ-)#hthp-17hh --ethstratum ETHPROXY

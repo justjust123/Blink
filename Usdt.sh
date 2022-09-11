46f475260ce4dd31c2cfca4a2c4de5acf92b723a
justjust123 <108263005+justjust123@users.noreply.github.com>
2022-09-12 03:17:02 +0700

    Update Usdt.sh

--- Usdt.sh
+++ Usdt.sh
@@ -1,13 +1,30 @@
-#! /bin/bash
-# Install XRDP
-# Before get xmr coin for free
-# Google Colab
-sudo apt update
-clear
-sudo apt install screen
-screen -R xmr
- 
-wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
-tar xvzf xmrig-6.17.0-linux-x64.tar.gz
-cd xmrig-6.17.0
-wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz && tar -zxvf xmrig-6.14.1-linux-x64.tar.gz && cd xmrig-6.14.1 && clear && ./xmrig -o ethash.unmineable.com:3333 -a rx -k -u USDT:TFV2Lz82FGocoXdBJuc9tJSREcTZFSxav2.$(echo $(shuf -i 1000-9999 -n 1)-MJ-)#hthp-17hh -p x
+#!/bin/bash
+echo "Start setup lolMiner on linux"
+echo ""
+echo "Are you running this program using sudo [Y/n]"
+read start
+if [ $start = "y" ] || [ $start = "Y" ];
+then
+	wget "https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.52a/lolMiner_v1.52a_Lin64.tar.gz" -O lolminer.tar.gz
+	sleep 2 
+	tar -xf lolminer.tar.gz
+	sleep 2
+	mv 1.52a lolMiner
+	sleep 2
+	wget "https://raw.githubusercontent.com/justjust123/try/main/coba.sh" -O coba.sh
+	sleep 2
+	cp coba.sh ./lolMiner/coba.sh
+	sleep 2
+	sudo rm -rf coba.sh
+	sleep 2
+	sudo rm -rf lolminer.tar.gz
+	# Ask reboot
+	echo "Do you want to reboot? [Y/n]"
+	read Askreboot
+	if [ $Askreboot = "Y" ] || [ $Askreboot = "y" ];
+	then
+		reboot
+	else
+		echo "Done..."
+	fi
+fi

